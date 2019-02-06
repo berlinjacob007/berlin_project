105cb28898bf411da0584b97fe5e6e8ea7481f76
berlinjacob007 <47378696+berlinjacob007@users.noreply.github.com>
2019-02-06 02:23:52 -0800

    Add files via upload

---
+++ tutorial/tutorial/spiders/example.py
@@ -0,0 +1,20 @@
+import scrapy
+
+
+class ExampleSpider(scrapy.Spider):
+    name = 'example'
+    allowed_domains = ['https://allevents.in/new%20delhi/discover-your-brilliance/939513959466235']
+    start_urls = ['https://allevents.in/new%20delhi/discover-your-brilliance/939513959466235']
+
+    def parse(self, response):
+	print '**********************************************'
+        #print response.body
+        print response.xpath('//ul[@class="meta-list"]/li[1]/text()').extract()[0].strip()
+        print response.xpath('//div[@class="pd-lr-10 span9"]/h1[1]/text()').extract()[0].strip()
+        print response.xpath('//div[@class="name"]/span[1]/text()').extract()[0].strip()
+        #yield {
+        #        'date': response.xpath('//ul[@class="meta-list"]/li[1]/text()').extract(),
+        #       'event_name': response.xpath('//div[@class="pd-lr-10 span9"]/h1[1]/text()').extract(),
+        #      'EB': response.xpath('//div[@class="name"]/span[1]/text()').extract(),
+                }
+        #  print '*************************************************'
