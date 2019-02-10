0fbb6d6158f8078e2ff9eca9c2016739b7b34f7a
berlinjacob007 <47378696+berlinjacob007@users.noreply.github.com>
2019-02-10 05:24:35 -0800

    Update example.py

--- tutorial/tutorial/spiders/example.py
+++ tutorial/tutorial/spiders/example.py
@@ -18,9 +18,15 @@ class ExampleSpider(scrapy.Spider):
                 'description':response.xpath('//*[@id="event-detail-fade"]/div[3]/div[1]/div[1]/div[2]/text()').getall(),
                 'venu':response.xpath('/html/body/div[5]/div[1]/div/div[2]/div[1]/div[2]/ul/li[3]/p/span[1]/text()').get() ,
                 'fw':response.xpath('//*[@id="event-detail-fade"]/div[3]/div[2]/div[2]/div[2]/div[3]/div/a/text()').get(),
-                'img_lnk1':response.xpath('//*[@id="event-detail-fade"]/div[1]/div[1]/img/@src').extract(),
-                'img_lnk2':response.xpath('//*[@id="event-detail-fade"]/div[3]/div[1]/div[1]/div[1]/img/@src').extract(),
+                'image_lnk1':response.xpath('//*[@id="event-detail-fade"]/div[1]/div[1]/img/@src').extract(),
+                'image_lnk2':response.xpath('//*[@id="event-detail-fade"]/div[3]/div[1]/div[1]/div[1]/img/@src').extract(),
                 
 
               }
 
+                  #print '**********************************************'
+                  #print response.body
+                  #print response.xpath('//ul[@class="meta-list"]/li[1]/text()').extract(),
+                  #print response.xpath('//div[@class="pd-lr-10 span9"]/h1[1]/text()').extract(),
+                  #print response.xpath('//div[@class="name"]/span[1]/text()').extract(),
+	          #print '**********************************************'
