41cea79a61fc0abdf40c2ce0e84bdb5c027fb902
berlinjacob007 <47378696+berlinjacob007@users.noreply.github.com>
2019-02-11 08:12:47 -0800

    Update example.py

--- tutorial/tutorial/spiders/example.py
+++ tutorial/tutorial/spiders/example.py
@@ -19,7 +19,7 @@ class ExampleSpider(scrapy.Spider):
                 'venu':response.xpath('/html/body/div[5]/div[1]/div/div[2]/div[1]/div[2]/ul/li[3]/p/span[1]/text()').get() ,
                 'fw':response.xpath('//*[@id="event-detail-fade"]/div[3]/div[2]/div[2]/div[2]/div[3]/div/a/text()').get(),
                 'image_lnk1':response.xpath('//*[@id="event-detail-fade"]/div[1]/div[1]/img/@src').extract(),
-                'image_lnk2':response.xpath('//*[@id="event-detail-fade"]/div[3]/div[1]/div[1]/div[1]/img/@src').extract(),
+                'img_lnk2':response.xpath('//*[@id="event-detail-fade"]/div[3]/div[1]/div[1]/div[1]/a/@data-image').extract(),
                 
 
               }
